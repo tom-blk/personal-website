7e9b3459dc9a70d72df96386a11f53108d26009f
tom-blk <tom.bleek@proton.me>
2024-03-26 14:51:25 +0100

    Changed functionality of TechnologyDetailsCard on large screens

--- app/components/cards/technology-details-card/TechnologyDetailsCard.Component.tsx
+++ app/components/cards/technology-details-card/TechnologyDetailsCard.Component.tsx
@@ -12,19 +12,19 @@ interface Props{
 
 const generalClasses = "p-4 z-30 bg-black bg-opacity-90 border border-slate-800 rounded-md text-sm "
 const mobileClasses = "animate-fade-in-out fixed inset-x-0 mx-auto bottom-5 text-xs text-left w-max "
-const desktopClasses = "lg:animate-none lg:absolute lg:right-full lg:bottom-auto lg:text-sm"
+const desktopClasses = "lg:text-sm"
 
 
 const TechnologyDetailsCard = (props: Props) => {
   const {professionalExperience, generalExperience, positions, projects} = props;
 
   return (
-    <section className={generalClasses + mobileClasses + desktopClasses}>
+    <article className={generalClasses + mobileClasses + desktopClasses}>
       <div>Professional Experience: {professionalExperience}</div>
       <div>General Experience: {generalExperience}</div>
       <div>Positions: {positions}</div>
       <div>Projects: {projects}</div>
-    </section>
+    </article>
   )
 }
 
