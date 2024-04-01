7176fea28412778e5a3b9844926e9c1ecc09b436
tom-blk <tom.bleek@proton.me>
2024-04-01 13:34:40 +0200

    Added more professional image

--- app/components/generic/avatar/Avatar.Component.tsx
+++ app/components/generic/avatar/Avatar.Component.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Avatar = () => {
 
-    const img = 'images/me.png'
+    const img = 'images/me.jpeg'
 
   return (
     <div className={"h-28 w-28 rounded-full overflow-hidden lg:h-36 lg:w-36"}>
