8118870414ec094fb5495a8e1ca14950f31db810
Tom Bleek <tom.bleek@proton.me>
2024-04-17 11:37:16 +0200

    Added cache for fetching technologies

--- app/api/fetch.ts
+++ app/api/fetch.ts
@@ -1,4 +1,5 @@
 import { query } from '@/app/db/db';
+import { TechnologyCache } from '../cache/technology-cache';
 
 export const getAllProjects = async () => {
     try{
@@ -23,6 +24,19 @@ export const getAllTechnologies = async () => {
         const data = await query('SELECT * FROM Technologies');
         return data.rows;
     } catch(error){
+        console.error("Error while fetching Data: ", error);
+    }
+}
+
+export const getSingleTechnology = async (id: number) => {
+    if(TechnologyCache.has(id)){
+        return TechnologyCache.get(id);
+    }
+
+    try{
+        const data = await query('SELECT * FROM Technologies WHERE id = $1', [id]);
+        return data.rows[0];
+    }catch(error){
         console.error("Error while fetching Data: ", error);
     }
 }
---
+++ app/cache/technology-cache.ts
@@ -0,0 +1,1 @@
+export const TechnologyCache = new Map();
